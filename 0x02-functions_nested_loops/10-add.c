a90d39b2a0086150327491c64b6be8fda04eec72
JosephNjorog <njorojoe11173@gmail.com>
2023-09-13 19:36:09 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 
 /**
  * add - Adds two values
- * @n1: The first number to be computed 
+ * @n1: The first number to be computed
  * @n2: The second number to be computed
  * Return: Integer value
  */
