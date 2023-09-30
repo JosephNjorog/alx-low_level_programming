0094bfb4768f04c57c9c5752cbd2663fbd94e462
JosephNjorog <njorojoe11173@gmail.com>
2023-09-30 05:24:42 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper - checks for upewrcase.
+ *@c: argument being checked.
+ *Return: 1 if uppercase.
+ *	  0 if not.
+ */
+
+int _isupper(int c)
+{
+	while (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
