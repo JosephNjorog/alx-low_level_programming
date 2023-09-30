096c104f92b1c58ee5866ee9749402bdd1c629b3
JosephNjorog <njorojoe11173@gmail.com>
2023-09-30 05:29:48 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiplies intagers.
+ *@a: firt int.
+ *@b: seccond int.
+ *Return: intager multiple product (prod).
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
