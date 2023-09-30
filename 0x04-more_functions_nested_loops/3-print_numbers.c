4770ea40b609114fcfd1d7139c5de5ba44b316a6
JosephNjorog <njorojoe11173@gmail.com>
2023-09-30 05:31:31 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ *print_numbers - prints numbers.
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
