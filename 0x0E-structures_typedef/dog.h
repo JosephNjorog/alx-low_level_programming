0489cf8189b6e5addb6786b752e7807a1a10b10b
JosephNjorog <njorojoe11173@gmail.com>
2023-10-10 14:41:02 -0700

    0x0E. C - Structures, typedef codes for my files haha

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
