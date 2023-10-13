2422c35e61f65e9c1cc994ddbfd4d37e03a433b4
JosephNjorog <njorojoe11173@gmail.com>
2023-10-13 14:21:05 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints the name
+ * @name: name to print
+ * @f: function parameter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+0x0F-function_pointers readme file for dem codes and damn its fun yeepeeeeey
