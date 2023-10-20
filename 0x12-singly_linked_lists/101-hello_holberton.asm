546f013cd436320833f9c140fedd21329e578732
JosephNjorog <njorojoe11173@gmail.com>
2023-10-20 16:17:59 -0700

    0x12. C - Singly linked lists guess who has done all the tasks and is hapy hahahaha its me mem me me both mandatory and advanced and ive pushed them with this silly commit message dah lol

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,19 @@
-    global    main
-    extern    printf
+section .data
+    hello db "Hello, Holberton", 0Ah, 0
+
+section .text
+    global main
+    extern printf
+
 main:
-    mov   edi, format
-    xor   eax, eax
-    call  printf
-    mov eax, 0
-	ret
-format: db `Hello, Holberton\n`,0
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [hello]
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
