a58c0fbe6f846112593e8fdb3eb794bc5f795457
JosephNjorog <njorojoe11173@gmail.com>
2023-10-20 16:02:52 -0700

    0x12. C - Singly linked lists guess who has done all the tasks and is hapy hahahaha its me mem me me both mandatory and advanced and ive pushed them with this silly commit message dah lol

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
