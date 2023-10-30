66c4d480064a7cfd914dbb332fb7fe229eb4b949
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-10-30 18:43:47 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <elf.h>
 
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
 ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
