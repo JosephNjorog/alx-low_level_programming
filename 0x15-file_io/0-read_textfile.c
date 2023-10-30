ed5febad790e33e869dd78723ee7ef76865f0573
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-10-30 17:35:33 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <fcntl.h>
+#include <unistd.h>
 /**
  * read_textfile - function reads a text file
  * @filename: parameter 1
  * @letters:parameter function 2
  * Return: 0
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+
+int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_read, bytes_written;
-	char *buffer;
+    if (filename == NULL)
+        return (-1);
 
-	if (filename == NULL)
-		return (0);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd == -1)
+        return (-1);
 
+    if (text_content != NULL)
+    {
+        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes_read = read(fd, buffer, letters);
-
-	if (bytes_read == -1)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	close(fd);
-	free(buffer);
-
-	return (bytes_written);
+    close(fd);
+    return (1);
 }
