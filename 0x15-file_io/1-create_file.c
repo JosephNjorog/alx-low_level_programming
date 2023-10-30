c311e79c04eb2eb5a5bfe64d4b819e4cf6248afd
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-10-30 18:09:02 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <string.h>
 
 /**
- * create_file - Creates a file with the specified conditions.
- * @filename: Name of the file to create.
- * @text_content: A NULL-terminated string to write.
+ * _strlen - count number of characters in a string
+ * @s: given string
  *
- * Return: 1 on success, -1 on failure
+ * Return: Length
  */
+size_t _strlen(char *s)
+{
+	size_t i;
 
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - Creates file and adds text to it.
+ * @filename: Name of the file
+ * @text_content: Content to be added to file.
+ *
+ * Return: 1 (Success) | -1 (Failure)
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_result;
+	int fd;
 
-	if (filename == NULL)
-	return (-1);
+	if (!filename)
+		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-	return (-1);
+		return (-1);
 
-	if (text_content != NULL)
-	{
-	write_result = write(fd, text_content, strlen(text_content));
-	if (write_result == -1)
-	{
-	close(fd);
-	return (-1);
-	}
-	}
-
+	if (text_content)
+		if (write(fd, text_content, _strlen(text_content)) < 0)
+			return (-1);
 	close(fd);
 	return (1);
-
 }
