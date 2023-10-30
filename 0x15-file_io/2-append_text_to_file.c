8042baae6817edc5201c6843b892d63d92840488
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-10-30 18:16:28 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,42 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text to the end of file.
- * @filename: Name of the file.
- * @text_content: The NULL-terminated string
+ * _strlen2 - Counts number of characters in a string
+ * @s: given string
  *
- * Return: 1 on success and -1 on failure.
+ * Return: Length of the string.
  */
-
-int append_text_to_file(const char *filename, char *text_content)
-
+size_t _strlen2(char *s)
 {
-	int fd, write_result;
+	size_t i;
 
-	if (filename == NULL)
-	return (-1);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file.
+ * @text_content: Content to be added at the end of file.
+ *
+ * Return: 1 (Success) | -1 (Failure)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
 
-	if (fd == -1)
-	return (-1);
+	if (!filename)
+		return (-1);
 
-	if (text_content != NULL)
-	{
-	write_result = write(fd, text_content, strlen(text_content));
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd < 0)
+		return (-1);
 
-	if (write_result == -1)
-	{
-	close(fd);
-	return (-1);
-	}
-	}
-
+	if (text_content)
+		if (write(fd, text_content, _strlen2(text_content)) < 0)
+			return (-1);
 	close(fd);
 	return (1);
 }
