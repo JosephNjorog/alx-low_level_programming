6f230becc2aead9db0ed14429cebc1bb8b4e53f8
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-10-31 08:32:24 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * read_textfile - Reads and prints text from a file to stdout.
- * @filename: Name of the file.
- * @letters: Number of letters to be read and printed.
- *
- * Return: Actual number of read and printed letters.
+ * read_textfile -Reads and prints text from a file to stdout.
+ * @filename:Name of the file.
+ * @letters:Number of letters to be read and printed.
+ * Return:Actual number of read and printed letters.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
