c42c4b3c629581c7ef31c15db85d8c745f80f6f0
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-11 18:28:51 +0300

    Create 3-islower.c

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+
+int islower('H');
+
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+
+}
