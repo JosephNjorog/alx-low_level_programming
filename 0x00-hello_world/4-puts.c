314997553479b7808e86b7d29a52b821a451a2ce
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-11 12:40:16 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print string
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
