8d014fcd7869ed5ac23917a6a7b9b6be1c07002f
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-20 02:14:52 +0300

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: hash table size (array's size)
+ *
+ * Return: pointer to hash table (Success) | NULL (Failure)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(*table));
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof((*table->array)) * size);
+	if (!(table->array))
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
